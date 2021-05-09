26e8df0f7656ec76fa49f525fcdc4f4983fc9788
Felipi Torres Santana <devfelipisan@gmail.com>
2021-05-09 00:15:53 -0300

    Add Project Maven
    Project update for automated unit test application

---
+++ src/test/java/ad1_poo_2021_1/Test_AD1_2021_1.java
@@ -0,0 +1,21 @@
+package ad1_poo_2021_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+    /**
+    * 
+    * @author Felipi Santana
+    * @since 08/05/2021
+    * 
+    */
+
+class Test_AD1_2021_1 {
+    
+    @Test
+    void createObjectAmigo() {
+        Amigo amigo = new Amigo("nome");
+        
+    }
+}
